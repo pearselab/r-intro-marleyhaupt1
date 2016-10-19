8cd1b025bfe08d38c9c1dbd449b995e1a78ee631
Marley Jade Haupt <marleyjadehaupt@Marleys-MacBook-Air.local>
2016-10-19 11:36:12 -0600

    Marleys Commit Retry

--- lesson_1.R
+++ lesson_1.R
@@ -9,6 +9,71 @@ c(c(1,2,3), c(4,5,6))
 #     - they need to be of the same type, though!
 c(1,2, "three")
 
+## cat - concatenate and print
+  # - converts arguments to character vectors and concatenates them to a single character vector
+
+## cbind - combine R objects by rows or columns
+  # - takes a seq of vector, matrix, or df arguments and combines them by col or rows
+
+## col, row - column and row indexes
+  # - returns a matrix of integers indicating their row/column in a matrrix like object
+
+## cut - convert numberic to factor
+  # - divides the range of "x" into intervals and codes
+
+## diff
+
+## dim
+
+## rownames, colnames, names
+
+## expand.grid
+
+##eigen,%*%, lower.tri, upper.tri, diag(try?"%*%", not?%*%, as this is an operator, and document the function, not the maths)
+
+## gl
+
+## identical
+
+## image
+
+## library
+
+## jitter
+
+## ls; what does rm(list=ls(()) do?
+
+## mean, median, max, min
+
+## paste
+
+## read.csv, reat.table, write.csv, write.table
+
+## rnorm, pnorm, dnorm, qnorm
+
+## runif, rpois
+
+## rank
+
+## sort, rank, order
+
+## outer
+
+## rep
+
+## rowSum, colSum
+
+## seq
+
+## source
+
+## which, which.min, which.max
+
+## setdiff, intersect, union
+
+## table
+
+## with (look at the examples because the technical explanation is way too smart for me)
 ################################################
 ## Bonus exercises #############################
 ################################################
