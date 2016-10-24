20b5893b535062e5b1802f4a9aa2dbd323827f09
Marley Jade Haupt <marleyjadehaupt@Marleys-MacBook-Air.local>
2016-10-24 11:18:11 -0600

    Need help with Question 3

---
+++ r-exercise2-marleyhaupt1.R
@@ -0,0 +1,22 @@
+## 1. Write a loop that prints out the numbers from 20 to 10
+for (i in 20:10){
+  print(i)
+}
+## 2. Write a loop that prints out only the numbers 20 to 10 that are even
+for (i in 20:10){
+  if (i %% 2 == 0){
+    print(i)
+    }
+} 
+## 3. Write a function that calculates whether a number is a prime number
+prime<-function(x){
+  for (i in x-1:2){
+    x %% i
+  }
+  if(x == 0){
+    print("No")
+    }
+    else{
+      print("Yes")
+    }
+  }
