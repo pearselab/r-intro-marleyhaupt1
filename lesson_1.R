653c0cacd9f9c1c56cc5845973e8c7f85768be6f
Marley Jade Haupt <marleyjadehaupt@Marleys-MacBook-Air.local>
2016-10-24 09:41:57 -0600

    test

--- lesson_1.R
+++ lesson_1.R
@@ -63,26 +63,81 @@ colnames(y)<-c("ones","one to four")
 rownames(y)<-c("ur","mom","goes 2","college")
 
 
-##### expand.grid
+##### expand.grid - Create a Data Frame from all combinations of factor variables
+  # - Takes a vector or factor and turns it into a dataframe! Yay!
+x<-seq(0,100,by=10)
+y<-seq(-10,10,by=1)
+expand.grid(x,y)
+head(expand.grid(x,y))
 
-#####eigen,%*%, lower.tri, upper.tri, diag
-  #####(try?"%*%", not?%*%, as this is an operator, and document the function, not the maths)
+##### eigen - Spectral Decomposition of a Matrix
+  # - Computes eigenvalues and eigenvectors of numeric or complex matrices
+v<-cbind(c(1,-1),c(-1,1))
+eigen(v)
 
-##### gl
+##### %*% - Matrix Multiplication
+  # - Multiplies two matrices if they are conformable. Vectors become either rows or columns
+x<-matrix(1,2,2)
+y<-matrix(1:4,2,2)
+x %*% y
 
-##### identical
+##### lower.tri, upper.tri - Lower and Upper Triangular Part of Matrix
+  # - Turns the values of a matrix to TRUEs or FALSEs with the TRUEs in either the "lower" or "upper" triangle
+m2<-matrix(1:20,4,5)
+lower.tri(m2)
+upper.tri(m2)
 
-##### image
+##### diag - Matrix Diagonals
+  # - Used for 4 things: (1) if x is a matrix it extracts the diagonal (2) if x is missing and nrow is specified 
+  # - it returns an identify matrix (3) if x is a scalar and the only argument, it returns a square identity matrix
+  # - given by the scalar (4) if x is a numeric vector with a length >2 or with further arguements, it returns
+  # - a matrix with the given diagonal and zero off-diagonal entries
+x<-matrix(1:20,4,5)
+diag(x)
+diag(NA,nrow=2)
+diag(3)
+y<-c(1,2,3,4)
+diag(y)
 
-##### library
+##### gl - Generate Factor Levels
+  # - Generates factors by specifying the pattern of their levels
+gl(5,4,20)  ## - Generates the numbers 1:5 in sets of 4 (i.e., 1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5) w/Levels 1:5
+gl(2,1,10,labels=c("Marley", "Haupt")) ## [1] Marley Haupt Marley Haupt Marley Haupt (10 times) w/ Levels Marley and Haupt
 
-##### jitter
+##### identical - Test Objects for Exact Equality
+  # - A good way to check if two objects are exactly equal. If so, then TRUE if not then FALSE. 
+  # - Even checks the type (i.e. double vs. integer)
+x<-matrix(1:20,4,5)
+y<-c(1,2,3,4)
+identical(x,y) #[1] FALSE
+y<-matrix(1:20,4,5)
+identical(x,y) #[1] TRUE
+
+##### image - Display a Color Image
+  # - Can be used to visualize 3D spatial data (aka images). Creates a grid of colored or gray-scale rectangles c
+  # - corresponding to values of z
+image(t(volcano)[ncol(volcano):1,]) # I tried to think of an example that I could make up myself 
+x<-y<-seq(-4*pi, 4*pi, len=27)      # but I wasn't sure where to start
+r<-sqrt(outer(x^2,y^2,"+"))
+image(z=z<-cos(r^2)*exp(-r/6),col=gray((0:32)/32))
+
+##### library - Loading/Attaching and Listing of Packages
+  # - The command that loads a package so you can use certain commands that are otherwise unavailable
+library(rjags)            # Once rjags is loaded I can create JAGS models in R and run MCMC. 
+library(lib.loc=.Library) # Lists all of the packages available
+
+##### jitter - Add Noise ('Jitter') to Numbers
+  # - Add a small amount of noise to a numeric vector
+  # - jitter(x,...) returns a numeric of the same length as x, but with an amount of noise added in order to break ties
+x<-seq(1,10,1)
+# [1] 1 2 3 4 5 6 7 8 9 10
+jitter(x)
+# [1] 1.095372 2.032013 3.081988 3.973232 5.166270 5.803110 7.132575 7.885574 9.139420 9.956630
 
 ##### ls; what does rm(list=ls(()) do? - List Objects
   # - rm(list=ls()) erases R's memory of the names of objects you have previously called out
   # - ls shows what data sets and functions have been defined by the user (when invoked with no argument)
   # - ls returns the names of the functions local variables (when invoked with no argument inside of a function)
-
 marleyfunc<-function(){y<-1;ls()}
 marleyfunc
 #function(){y<-1;ls()}  shows "y"
@@ -96,7 +151,7 @@ x
 q
 #Error: object 'q' not found
 
-## mean, median, max, min - Arithmetic Mean, Median Value, Maxima and Minima
+##### mean, median, max, min - Arithmetic Mean, Median Value, Maxima and Minima
   # - Generic function for the arithmetic mean, computing the sample median, or returning the maximum and minimum input values
 m2<-matrix(1:12,3,4)
 mean(m2)
@@ -108,12 +163,12 @@ max(m2)
 min(m2)
 #[1] 1
 
-## paste - Concatenate Strings
+##### paste - Concatenate Strings
   # - allows you to concatenate vectors after converting to character
 family<-c("marley","seth","shelley","jeff","howard")
 paste(family,"smells bad")
 
-## read.csv, read.table, write.csv, write.table - Data Input and Output
+##### read.csv, read.table, write.csv, write.table - Data Input and Output
   # - allows you to input data into R from another file 
   # (i.e., a comma separated excel spreadsheet or .csv)
   # make sure you set the correct working directory first or it won't work
@@ -122,7 +177,7 @@ read.csv("genotypes.csv")
 family<-c("marley","seth","shelley","jeff","howard")
 write.csv(family,"family.csv")
 
-## rnorm, pnorm, dnorm, qnorm - The Normal Distribution
+##### rnorm, pnorm, dnorm, qnorm - The Normal Distribution
   # - Density, distribution, quantile, and pseudo random generation for a normal distribution
 test<-rnorm(50,5,1)
 head(test)
@@ -131,32 +186,41 @@ x=seq(1,20,by=0.5)
 test2<-dnorm(x,mean=10,sd=1)
 plot(test2)
 
-## runif, rpois - Uniform Distribution and Poisson Distribution
+##### runif, rpois - Uniform Distribution and Poisson Distribution
   # - similar to rnorm, but for a uniform and poisson distribution, respectively
 rpois(n=100,lambda=10)
 runif(n=100,min=1,max=10)
 
-## rank
+##### rank - Sample Ranks
+  # - Ranks the values inside of a vector (from smalles to largest)
+x<-c(3,1,4,15,92)
+rank(x)
+# [1] 2 1 3 4 5
 
-## sort, rank, order
+##### sort, order - Sorting or Ordering Vectors
+  # - sort (or order) a vector or factor into ascending or descending order
+x<-c(1,7,5,6,2,3,9,10,8,4)
+sort(x)
+# [1]  1  2  3  4  5  6  7  8  9 10
 
-## outer
+##### outer - Outer Product of Arrays
+  # - 
 
-## rep
+##### rep
 
-## rowSum, colSum
+##### rowSum, colSum
 
-## seq
+##### seq
 
-## source
+##### source
 
-## which, which.min, which.max
+##### which, which.min, which.max
 
-## setdiff, intersect, union
+##### setdiff, intersect, union
 
-## table
+##### table
 
-## with (look at the examples because the technical explanation is way too smart for me)
+##### with (look at the examples because the technical explanation is way too smart for me)
 
 
 ################################################
