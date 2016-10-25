8ec54d87097dc68fbd43b50c86c033d5af116dd1
Marley Jade Haupt <marleyjadehaupt@Marleys-MacBook-Air.local>
2016-10-25 11:36:52 -0600

    Typed Questions, Tried 4

--- r-exercise2-marleyhaupt1.R
+++ r-exercise2-marleyhaupt1.R
@@ -28,8 +28,99 @@ for (i in 1:20){
   f<-i %% 5
   if(f == 0){
     print("Good: NUMBER")
+    print(i)
   }
-  for (p in 2:(p-1)){
-    r<-
+  for (p in 2:(i-1)){
+    g<- i %% p
+    if(g == 0){
+    } else{
+      print("Job: NUMBER")
+      print(i)
+    }
   }
 }
+
+## 5. A biologist is modelling population growth using a Gompertz curve, which
+  # is defined as y(t) = a.e^−b.e^−c.t where y is population size, t is time,
+  # a and b are parameters, and e is the exponential function. Write them a 
+  # function that calculates population size at any time for any values of its
+  # parameters.
+
+## 6. The biologist likes your function so they want you to write another
+  # function that plots the progress of the population over a given length of
+  # time. Write it for them.
+
+## 7. The biologist has fallen in love with your plotting function, but wants
+  # to color y values above a as blue and y values above b as red. Change your
+  # function to allow that.
+
+## 8. You are beginning to suspect the biologist is taking advantage of you.
+  # Modify your function to plot in purple any y value that is above a and b.
+  # Hint: Try putting 3==3 & 2==2 and 3==4 | 2==2 into an if statement and see
+  # what you get. Using this construction may make this simpler.
+
+## 9. Write a function that draws boxes of a specified height and look like this
+  # (height 3, width 5):
+  # *****
+  # *   *
+  # *****
+
+## 10. Modify your box function to ut text centered inside the box, like this:
+  # *****************
+  # *               *
+  # *   some text   *
+  # *               *
+  # *****************
+
+## 11. Modify your box function to build boxes of arbitrary text, taking the
+  # demensions specified in terms of demensions, not the text. For example,
+  # box ("wdp", 3, 9, "hey") might produce:
+  # wdpwdpwdp
+  # w  hey  w
+  # wdpwdpwdp
+
+## 12. In ecology, hurdle models are often used to model the abundance of species
+  # found on surveys. They first model the probability that a species will be
+  # present (drawn, for example, from the Poisson distribution). Write a function
+  # that simulates the abundance of a species at n sites given a probability of
+  # presence (p) and that its abundance is drawn from a Poisson with a given 
+  # (lambda). Hint: there is no ernoulli distribution in R, but the Bernoulli is
+  # a special case of what distribution?...
+
+## 13. An ecologist really likes your hurdle function (will you never learn?). 
+  # Write them a function that simulates lots of species (each with their own p 
+  # and lambda) across n sites. Return the results in a matrix where each species
+  # is a column, and each site a row (this is the standard used for ecology data 
+  # in R).
+
+## 14. Professor Savitzky approaches you with a delicate problem. A member of faculty
+  # became disoriented during fieldwork, and is now believed to be randomly
+  # wandering somewhere in the desert surrounding Logan. He is modelling their 
+  # progress through time in five minute intervals, assuming they cover a random,
+  # Normally-distributed distance in latitude and longitude in each interval. Could
+  # you simulate this process 100 times and plot it for him?
+
+## 15. Professor Savitzky is deeply concerned to realise that the member of faculty
+  # was, in fact, at the top of a steep mountain in the fog. Approximately 5 miles
+  # away, in all directions, from the faculty member's starting point is a deadly
+  # cliff! He asks if you could run your simulation to see how long, on average,
+  # until the faculty member plummets to their doom.
+
+## 16. Sadly, by the time you have completed your simulatiosn the faculty member
+  # has perished. Professor Savitzky is keep to ensure this will never happen again,
+  # and so has suggested each faculty member be attached, via rubber band, to a 
+  # pole at the center of the site whenever conducting fieldwork. He assures you
+  # that you can model this by assuing that the faculty member, at each time-step
+  # moves alpha * distance-from-pole latitudinally and longitudinally (in 
+  # additon to the rate of movement you've already simulated) each time-step.
+  # Simulate this, and see how strong the rubber band (alpha) must be to keep
+  # the faculty member safe for at least a day.
+
+## 17. (If you finish early: Most, if not all, faculty members are not as young
+  # as they once were. See what effect the faculty member tiring (and 
+  # eventually sitting down and giving up) would have on their likelihood of
+  # survival. What would happen if a faculty member panicked and walked faster
+  # through time?)
+
+## Bonus Exercises: Your loops will go a lot faster in R if you pre-allocate your output
+  # variables....
