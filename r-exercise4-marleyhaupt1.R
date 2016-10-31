21b7080e5a5eca4561081d3ece17dcbca9639d01
Marley Jade Haupt <marleyjadehaupt@Marleys-MacBook-Air.local>
2016-10-31 10:51:33 -0600

    Random Number Generator

--- r-exercise4-marleyhaupt1.R
+++ r-exercise4-marleyhaupt1.R
@@ -2,11 +2,12 @@
   # random numbers. Use it to create a dataset of 10 variables, each drawn
   # from a Normal distribution with different means and variances. This can
   # be achieved on one line.
-replicate(10,rnorm(1,.Random.seed[1],.Random.seed[1]))
+replicate(10,rnorm(1,rnorm(1,10,1),runif(1,0,3)))
 
 # 2. Make your own version of the summary function for continuous datasets 
   # (like the one you generated above). You don't have to slavishly replicated
   # summary.data.frame; write something you would find useful.
+my.summary<-function(x)
 
 # 3. Write a summary function to summarise datasets containing only
   # categorical (...!is.numeric...) data.
